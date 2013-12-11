949c36303d6a481a1b5c2c1b21ddab88a150d939
Luca Lucci <l.lucci@spinitalia.com>
2013-12-11 13:10:54 +0100

    Versione 3.1
      - Eliminati tutti i riferimenti al braccio robotico. Ora questo modulo si occupa solo di rigirare i dati canbus sulla ethernet
    
      - Modificato il file di compilazione: da ora in poi il file di uscita si chiamerà progetto_cbrn.o invece di centrale_controllo_stdof.o

--- Centrale_Controllo_PC/main.c
+++ Centrale_Controllo_PC/main.c
@@ -12,8 +12,6 @@
 #include <time.h>
 #include <errno.h>
 
-#include <libudev.h>
-
 #include <locale.h>
 
 #include <string.h>
@@ -28,18 +26,10 @@
 
 #include "socket_can_interface.h"
 #include "pc_interface_udp.h"
-#include "arm_udp.h"
-#include "rs232.h"
 
 #define CCU_ADDRESS "192.168.1.102"
 #define CCU_PORT 8010
 #define PC_INTERFACE_PORT 8011
-#define ARM_PORT 8012
-#define JOY_ADDRESS "192.168.1.60"
-#define JOY_PORT 8013
-
-#define TIMEOUT_SEC 0
-#define TIMEOUT_USEC 50000
 
 /* Macro */
 #undef max
@@ -62,16 +52,6 @@ int main()
   struct pc_interface_udp_frame pc_interface_buffer_temp;
   struct sockaddr_in pc_interface_client_address;
 
-  /* Arm interface */
-  int arm_client = -1;
-  struct arm_frame arm_buffer_temp;
-  struct sockaddr_in arm_client_address;
-  
-  /* rs232 interface */
-  int rs232_device = -1;
-  unsigned char rs232_buffer_temp[RS232_BUFFER_SIZE];
-  int i;
-  
   /* Generic Variable */
   int done = 0;  // for the while in main loop
   int bytes_read;  // to check how many bytes has been read
@@ -100,20 +80,6 @@ int main()
   else
     printf("Init pc interface client\t[OK]\n");
  
-   /* Init Arm Interface */
-  if(arm_open(&arm_client, &arm_client_address, ARM_PORT, JOY_ADDRESS, JOY_PORT) == 0)
-    perror("error connection");
-  else
-    printf("Init arm client\t[OK]\n");
- 
-  /* Init rs232 Interface */
-  rs232_device = com_open("/dev/ttyUSB1", 115200, 'N', 8, 1);
-
-  if(rs232_device == -1)
-    perror("com_open");
-  else
-    printf("Init rs232\t[OK]\n");
-	
   printf("Run main program. . .\n");
 
   while(!done)
@@ -147,31 +113,7 @@ int main()
         nfds = max(nfds, pc_interface_client);
       }
     }
-	
-	if(arm_client > 0)
-    {
-      FD_SET(arm_client, &rd);
-      nfds = max(nfds, arm_client);  
 
-      if(arm_buffer_tx_empty == 0)
-      {
-        FD_SET(arm_client, &wr);
-        nfds = max(nfds, arm_client);
-      }
-    }
-	
-	if(rs232_device > 0)
-    {
-      FD_SET(rs232_device, &rd);
-      nfds = max(nfds, rs232_device);  
-
-      if(rs232_buffer_tx_empty == 0)
-      {
-        FD_SET(rs232_device, &wr);
-        nfds = max(nfds, rs232_device);
-      }
-    }
-	
     select_result = select(nfds + 1, &rd, &wr, NULL, NULL);
 
     if(select_result == -1 && errno == EAGAIN)
@@ -238,72 +180,6 @@ int main()
 
       }  //if(FD_ISSET(pc_interface_client, &wr))
     }
-	
-	/* Manage rs232 message */
-    if(rs232_device > 0)
-    {
-      if(FD_ISSET(rs232_device, &rd))
-      {
-        // Read a message back from the CAN bus
-        bytes_read = rs232_read(rs232_device);
-
-        if(bytes_read < 0)
-          perror("rs232 read");
-		  
-		// load udp buffer only if I have received the whole message
-		if(rs232_check_last_char(0x0d))
-		{
-		  bytes_read = rs232_unload_rx(rs232_buffer_temp);
-		  
-		  if(bytes_read > 0)
-		  {
-		    rs232_buffer_temp[bytes_read] = 0;
-			
-		  	memcpy(arm_buffer_temp.frame, rs232_buffer_temp, bytes_read + 1);
-			arm_load_tx(arm_buffer_temp);
-		  }
-		}
-       
-      } // end if(FD_ISSET(socket_can, &rd))
-
-      if(FD_ISSET(rs232_device, &wr))
-      {
-	    bytes_sent = rs232_write(rs232_device);
-		
-		if(bytes_sent <= 0)
-          printf("Error on rs232_write");
-      }
-    }
-	
-	/* Redirect arm message */
-    if(arm_client > 0)
-    {
-      if(FD_ISSET(arm_client, &rd))
-      {
-        // Ethernet is fastet than rs232, so I don't need circular buffer on rx
-        bytes_read = recvfrom(arm_client, &arm_buffer_temp, sizeof(struct arm_frame), 0, NULL, NULL);
-
-        if(bytes_read > 0)
-		{
-		  //sprintf(arm_buffer_temp.frame, "%cRPA%c%c", 0x86, 0xd, 0);
-		  rs232_load_tx((__u8 *)arm_buffer_temp.frame, strlen(arm_buffer_temp.frame));
-		  //memcpy(rs232_buffer_temp, (__u8 *)arm_buffer_temp.frame, strlen(arm_buffer_temp.frame));
-		  //rs232_buffer_temp[strlen(arm_buffer_temp.frame)] = 0;
-		  //printf("[%i][%i]%s", strlen(arm_buffer_temp.frame),rs232_buffer_temp[0], &rs232_buffer_temp[1]);
-        }
-      } //if(FD_ISSET(arm_client, &rd))
-
-      if(FD_ISSET(arm_client, &wr))
-      {
-        //printf("%i\n", rs232_buffer_tx_data_count);
-        bytes_sent = arm_send(arm_client, &arm_client_address);
-
-        if(bytes_sent <= 0)
-          printf("Error on pc_interface_send");
-
-      }  //if(FD_ISSET(pc_interface_client, &wr))
-    }
-  
   }  // end while(!= done)
   
   return 0;
